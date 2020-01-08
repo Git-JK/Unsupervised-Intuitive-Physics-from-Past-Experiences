180db8dc23ecf4cd84641ba73da9ed8c2c8b0190
gzz <gzz_2000@126.com>
2020-01-08 11:12:37 +0800

    Added model parameters snapshot and image visualization

--- config.py
+++ config.py
@@ -2,8 +2,13 @@
 ## 训练设置
 
 cnt_epoch = 128
+batch_size = 32
 learning_rate = 0.001
 kl_weight = 0.005  # https://github.com/yzwxx/vae-celebA/blob/master/train_vae.py#L99
+
+log_every = 16
+save_snapshot_to = 'result/models'
+save_visualization_to = 'result/visualizations'
 
 ## 网络设置
 
--- train_action.py
+++ train_action.py
@@ -1,14 +1,11 @@
-from train_data import data
+from train_data import train_data, test_data, revert_preprocess
 from model import model_train, model_test
 import config
+import os
 
-def enum_train_data():
+def enum_train_data(batch_size):
     # to be implemented: to split data into batch
     raise NotImplementedError("enum_train_data not implemented")
-
-def enum_test_data():
-    # to be implemented: to get test data??
-    raise NotImplementedError("enum_test_data not implemented")
 
 def l2_loss(a, b):
     return tf.reduce_sum(tf.square(a - b))
@@ -18,13 +15,27 @@ def kl_loss(mean, logstdev):
 
 optimizer = tf.optimizers.Adam(learning_rate = config.learning_rate)
 
+tl.files.exists_or_mkdir(config.save_snapshot_to)
+tl.files.exists_or_mkdir(config.save_visualization_to)
+
 for epoch in range(config.cnt_epoch):
     print('epoch', epoch)
-    for step, (data_before, data_after) in enumerate(enum_train_data()):
+    
+    for step, (data_before, data_after) in enumerate(enum_train_data(config.batch_size)):
         model_train.train()
         with tf.GradientTape(persistent = True) as tape:
             outputs, mean, logstdev = model_train(data_before, data_after)
-            loss = config.kl_weight * kl_loss(mean, logstdev) + l2_loss(outputs, data_before)
+            loss = config.kl_weight * kl_loss(mean, logstdev) + l2_loss(outputs, data_after - data_before)
             print(step, loss)
         grad = tape.gradient(loss, model_train.trainable_weights)
         optimizer.apply_gradients(zip(grad, model_train.trainable_weights))
+        
+    if (epoch + 1) % config.log_every == 0:
+        model_train.save_weights(os.path.join(save_snapshot_to, 'model_train_' + str(epoch + 1) + '.h5'))
+        model_test.test()
+        # assuming test_data is just several images (~10)
+        test_data_after = model_test(test_data)
+        images = tf.concat([test_data, test_data_after + test_data], 0)
+        images = revert_preprocess(images)  # 逆向预处理（像素级）
+        tl.visualize.save_images(images, (2, test_data.shape[0]),
+                                 os.path.join(save_visualization_to, 'test_' + str(epoch + 1) + '.png'))
