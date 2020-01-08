85abcb90805975eed68267a869596fa545494065
JK <1800012933@pku.edu.cn>
2020-01-08 23:42:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -22,3 +22,21 @@ image_encoder_cnn = [('conv', 64), ('maxpool', 2),
                      ('conv', 64),
                      ('conv', 64), ('maxpool', 2),
                      ('conv', 32)]
+
+motion_encoder_cnn = [('conv', 96), ('maxpool', 4),
+                      ('conv', 96), ('maxpool', 2),
+                      ('conv', 128), ('maxpool', 2),  
+                      ('conv', 128), ('maxpool', 2),  
+                      ('conv', 256), ('maxpool', 2),
+                      ('conv', 256), ('maxpool', 2)]
+
+kernel_decoder = [4, 32, 32, 32, 5, 3, 5]
+
+kernel_decoder_decnn = [('deconv', 128), ('maxpool', 5),
+                        ('deconv', 128), ('maxpool', 5)]
+
+kernel_decoder_cnn = [('conv', 128), ('conv', 128), ('conv', 128)]
+
+motion_decoder_cnn = [('conv', 128), ('maxpool', 9),
+                      ('conv', 128),
+                      ('conv', 128)]
